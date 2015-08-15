39de0b95ea9db9b34e9aa418bf7f6852951d0af5
Brian Cox <bcox@hubspot.com>
2015-08-15 02:30:19 -0400

    Initial Commit

---
+++ Baseball/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Baseball
+//
+//  Created by Brian Cox on 8/15/15.
+//  Copyright © 2015 Brian Cox. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
