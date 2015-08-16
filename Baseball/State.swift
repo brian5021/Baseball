01cc711772e3aca55a071c351010e4943c83a85c
Brian Cox <bcox@hubspot.com>
2015-08-16 17:16:31 -0400

    State file actually

---
+++ Baseball/State.swift
@@ -0,0 +1,15 @@
+//
+//  State.swift
+//  Baseball
+//
+//  Created by Brian Cox on 8/16/15.
+//  Copyright © 2015 Brian Cox. All rights reserved.
+//
+
+import Foundation
+
+
+class State { // Class 'Point' has no initializers
+    var x: Double
+    var y: Double
+}
